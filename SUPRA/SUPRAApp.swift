1b2dba9a0dd9351980287d37345d5f7fd8b964e4
Nicolas Alonso <nicolas.alonsof84@gmail.com>
2026-07-17 11:53:36 +0200

    Initial Commit

---
+++ SUPRA/SUPRAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUPRAApp.swift
+//  SUPRA
+//
+//  Created by Nicolas Alonso on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SUPRAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
